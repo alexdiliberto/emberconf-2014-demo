9edc31ef197fdbac8c68ddb4f18799a8c58557ec
Nathan Hammond <nathan@nathanhammond.com>
2014-03-16 21:06:06 -0400

    Make traversing include all of the correct paths.

--- app/controllers/one-time-password/delivery.js
+++ app/controllers/one-time-password/delivery.js
@@ -1,3 +1,3 @@
 export default Ember.ArrayController.extend({
-  deliveryMethod: undefined
+  deliveryMethodId: undefined
 });
--- app/routes/one-time-password/register-device.js
+++ app/routes/one-time-password/register-device.js
@@ -2,7 +2,7 @@ export default Ember.Route.extend({
   actions: {
     submit: function() {
       // TODO: Make this actually communicate with the server.
-      this.replaceWith('one-time-password.register-device');
+      this.replaceWith('accounts');
     }
   }
 });

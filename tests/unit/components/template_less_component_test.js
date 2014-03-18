c5d4796c6b48228f789d79921d46f8e1466bbc7b
Nathan Hammond <nathan@nathanhammond.com>
2014-03-18 11:44:30 -0400

    Update to ember-cli 0.0.8 skeleton output. Remove straggling unnecessary components.

--- tests/unit/components/template_less_component_test.js
+++
@@ -1,8 +0,0 @@
-import { test , moduleForComponent } from 'emberconf-2014-demo/tests/helpers/module_for';
-
-moduleForComponent('template-less');
-
-test("template", function(){
-  var component = this.subject();
-  ok(this.$());
-});

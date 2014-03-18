d2f7306f3fe76ec246093ba14955e9c51ce8bff4
Alex DiLiberto <alex@alexdiliberto.com>
2014-03-18 17:56:27 -0400

    Remove unused variable

--- app/routes/statements.js
+++ app/routes/statements.js
@@ -5,7 +5,6 @@ export default Ember.Route.extend({
     scenario for testing and demonstration purposes only.
   */
   model: function() {
-    var self = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
       Ember.run.later(function() {
         reject({ stack: "Oh No! Statements route is slow and it causes an error." });

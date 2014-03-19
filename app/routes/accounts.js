f3a4c1dd5d0b6b1310cfe9167d22ca434a668e64
Alex DiLiberto <alex@alexdiliberto.com>
2014-03-18 21:05:10 -0400

    Quick update

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -3,15 +3,15 @@ export default Ember.Route.extend({
     return [{
         id: 1,
         name: 'Checking',
-        amount: 1000.00
+        amount: 1000
       },{
         id: 2,
         name: 'Savings',
-        amount: 5000.00
+        amount: 5000
       },{
         id: 3,
         name: 'CD',
-        amount: 750.50
+        amount: 750
       }
     ];
   }

cc8c30f558ee30e19e510e0fae6aa6bc93e171fe
Alex DiLiberto <alex@alexdiliberto.com>
2014-03-18 21:02:25 -0400

    More styling + cycle accounts button

--- app/routes/accounts/details.js
+++ app/routes/accounts/details.js
@@ -6,14 +6,23 @@ export default Ember.Route.extend({
     model hook to yeild the correct "account" object.
   */
   model: function(params) {
-    return this.modelFor('accounts').filter(function(acct) {
-      return acct.id == params.account_id;
-    })[0];
+    return this._filterAccountId(this.modelFor('accounts'), params.account_id);
+  },
+  _filterAccountId: function(array, x) {
+    return array.filter(function(account) { return account.id == x; })[0];
   },
   actions: {
     didTransition: function() {
       if(!this.currentModel) {
         this.transitionTo('accounts');
+      }
+    },
+    cycleAccounts: function(curAccountId) {
+      var model = this.modelFor('accounts');
+      if (curAccountId === -1 || curAccountId === model.length) {
+        this.transitionTo('accounts.details', this._filterAccountId(model, 1));
+      } else {
+        this.transitionTo('accounts.details', this._filterAccountId(model, curAccountId+1));
       }
     }
   }

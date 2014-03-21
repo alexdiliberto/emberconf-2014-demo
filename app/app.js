4ffca86eed02f50ee8342e8bac57bf27a589484b
Nathan Hammond <nathan@nathanhammond.com>
2014-03-21 01:32:04 -0400

    Start of conversion to injecting the session state into every route by default.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,13 @@
 import Resolver from 'ember/resolver';
+import session from 'emberconf-2014-demo/controllers/session';
+
+Ember.Application.initializer({
+  name: 'session',
+  initialize: function(container, app) {
+    container.register('session:main', session);
+    app.inject('route', 'session', 'session:main');
+  }
+});
 
 var App = Ember.Application.extend({
   LOG_ACTIVE_GENERATION: true,
---
+++ app/controllers/session.js
@@ -0,0 +1,5 @@
+export default Ember.Object.extend({
+  isIdentified: false,
+  isAuthenticated: false,
+  isAuthorized: false
+});

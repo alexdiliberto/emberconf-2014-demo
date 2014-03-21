f5f8b32ab4aac428171a4c5af609de7d8263c857
Nathan Hammond <nathan@nathanhammond.com>
2014-03-21 13:08:59 -0400

    Use HTML5 history.

--- app/router.js
+++ app/router.js
@@ -1,4 +1,7 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+Router.reopen({
+  location: 'history'
+});
 
 Router.map(function() {
   // Routes in authenticated space require full authentication.

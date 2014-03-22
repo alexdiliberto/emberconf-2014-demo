1db07d9fbbb1a07b5eec20dcac1122aa6f8e0ee4
Alex DiLiberto <alex@alexdiliberto.com>
2014-03-22 19:18:03 -0400

    fix for accounts model

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -1,23 +1,21 @@
 export default Ember.Route.extend({
   model: function() {
-    if(!this.modelFor('accounts')) {
-      return Ember.A([
-        Ember.Object.create({
-          id: 1,
-          name: 'Checking',
-          amount: 1000
-        }),
-        Ember.Object.create({
-          id: 2,
-          name: 'Savings',
-          amount: 5000
-        }),
-        Ember.Object.create({
-          id: 3,
-          name: 'CD',
-          amount: 750
-        })
-      ]);
-    }
+    return this.modelFor('accounts') || Ember.A([
+      Ember.Object.create({
+        id: 1,
+        name: 'Checking',
+        amount: 1000
+      }),
+      Ember.Object.create({
+        id: 2,
+        name: 'Savings',
+        amount: 5000
+      }),
+      Ember.Object.create({
+        id: 3,
+        name: 'CD',
+        amount: 750
+      })
+    ]);
   }
 });

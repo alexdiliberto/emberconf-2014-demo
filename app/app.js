7ec7e780b506ea0e7c9d4ea5779df89f0c6cd7bd
Alex DiLiberto <alex@alexdiliberto.com>
2014-03-21 23:45:08 -0400

    Commenting out logging for now to make reading the console easier

--- app/app.js
+++ app/app.js
@@ -10,11 +10,11 @@ Ember.Application.initializer({
 });
 
 var App = Ember.Application.extend({
-  LOG_ACTIVE_GENERATION: true,
-  LOG_MODULE_RESOLVER: true,
+  // LOG_ACTIVE_GENERATION: true,
+  // LOG_MODULE_RESOLVER: true,
   // LOG_TRANSITIONS: true,
   // LOG_TRANSITIONS_INTERNAL: true,
-  LOG_VIEW_LOOKUPS: true,
+  // LOG_VIEW_LOOKUPS: true,
   modulePrefix: 'emberconf-2014-demo', // TODO: loaded via config
   Resolver: Resolver['default']
 });

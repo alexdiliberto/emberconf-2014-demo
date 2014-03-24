66863d069cab32a170917d9cf556821e523f9924
Nathan Hammond <nathan@nathanhammond.com>
2014-03-24 08:52:59 -0400

    Update the third-party component.

--- app/components/third-party.js
+++ app/components/third-party.js
@@ -1,17 +1,16 @@
 export default Ember.Component.extend({
   tagName: ['a'],
-  attributeBindings: ['href'],
+  attributeBindings: ['download', 'href', 'hreflang', 'media', 'ping', 'rel', 'target', 'type'],
   action: '_trackExternalLink',
   _navigateToExternalLink: function(externalURL) {
     document.location = externalURL;
   },
   click: function(event) {
-    // For this example, I'm allowing 100ms for the analytics request to fire to prevent race
-    // condition situations and follow generic community accepted patterns.
-    // Please note: Certain analytics libraries have specific API calls to invoke exteral link
-    // tracking, so this could easily be changed to suit your needs.
-    //   e.g.: Google Analytics: https://support.google.com/analytics/answer/1136920?hl=en
+    // Stop normal link handling, while allowing propagation.
     event.preventDefault();
+
+    // Preferring to capture state here based upon the user's action as opposed to the state
+    // whenever Ember.run.later(); resolves.
     var url = this.get('href');
 
     // http://emberjs.com/guides/components/sending-actions-from-components-to-your-application/
@@ -20,10 +19,15 @@ export default Ember.Component.extend({
 
     // ...However, the following `this.triggerAction()` would work as well
     // To use this variation, simply update the analytics object "action name" to
-    // accept a componentContext paramter for any dynamic data retrieval
+    // accept a componentContext parameter for any dynamic data retrieval
     //  e.g.:_trackExternalLink: function(componentContext) { return { "external-url": componentContext.get('href') }; }
-    //this.triggerAction({ action: '_trackExternalLink' });
+    // this.triggerAction({ action: '_trackExternalLink' });
 
-    Ember.run.later(this, this._navigateToExternalLink, url, 100);
+    // For this example, I'm allowing 100ms for the analytics request to fire to prevent race
+    // condition situations and follow generic community accepted patterns.
+    // Please note: Certain analytics libraries have specific API calls to invoke external link
+    // tracking, so this could easily be changed to suit your needs.
+    //   e.g.: Google Analytics: https://support.google.com/analytics/answer/1136920?hl=en
+    Ember.run.later(this._navigateToExternalLink, url, 100);
   }
 });
